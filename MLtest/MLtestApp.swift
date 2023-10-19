edd27b52eb7006bd60f7be8c0952bf354832f7d0
Hasanen <hasanen.bak@gmail.com>
2023-10-19 22:59:51 +0200

    Initial Commit

---
+++ MLtest/MLtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLtestApp.swift
+//  MLtest
+//
+//  Created by Hasanen B on 2023-10-19.
+//
+
+import SwiftUI
+
+@main
+struct MLtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
